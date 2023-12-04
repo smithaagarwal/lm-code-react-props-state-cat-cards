d1a2b831d424d695e77d6a77324402700f2b8d96
smithaagarwal <smitha.kotian@gmail.com>
2023-12-04 14:39:30 +0000

    pass the cat data to the cat_card component

--- src/App.tsx
+++ src/App.tsx
@@ -92,7 +92,12 @@ function App(): JSX.Element {
       <main>
         <div className="cards__wrapper">
           {cats.map((cat) => (
-            <CatCard />
+            <CatCard
+              name={cat.name}
+              species={cat.species}
+              favFoods={cat.favFoods}
+              birthYear={cat.birthYear}
+            />
           ))}
         </div>
       </main>
--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -1,9 +1,15 @@
-const CatCard: React.FC = () => (
+interface CatCardProps {
+  name: string;
+  species: string;
+  favFoods: Array<string>;
+  birthYear: number;
+}
+const CatCard: React.FC<CatCardProps> = (props) => (
   <div className="card">
-    <h3 className="card__text card__header">Mr Grumpy</h3>
-    <p className="card__text">Species:Cat</p>
-    <p className="card__text">Favourite Food(s): Cavier</p>
-    <p className="card__text">Birth Year: 1982</p>
+    <h3 className="card__text card__header">{props.name}</h3>
+    <p className="card__text">{props.species}</p>
+    <p className="card__text">{props.favFoods}</p>
+    <p className="card__text">{props.birthYear}</p>
   </div>
 );
 
