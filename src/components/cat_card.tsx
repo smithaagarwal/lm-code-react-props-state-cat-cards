64c83196d7df9d81d614c981915a2118d2494a03
smithaagarwal <smitha.kotian@gmail.com>
2023-12-04 14:03:31 +0000

    create catCard component and cat data

---
+++ src/components/cat_card.tsx
@@ -0,0 +1,10 @@
+const CatCard: React.FC = () => (
+  <div className="card">
+    <h3 className="card__text card__header">Mr Grumpy</h3>
+    <p className="card__text">Species:Cat</p>
+    <p className="card__text">Favourite Food(s): Cavier</p>
+    <p className="card__text">Birth Year: 1982</p>
+  </div>
+);
+
+export default CatCard;
